8bc94a35ffcac2e12a6e7b40f0d34b01315e58e3
leo <757187389@qq.com>
2017-12-28 13:58:32 +0800

    更新了Hello.java文件

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ public class Hello{
 	
 	public static void main(String[] arg){
 			System.out.println("Java，你大爷");
+			
+			System.out.println("修改了1....");
+			System.out.println("修改了2....");
 	}
 	
 }
