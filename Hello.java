2d0381cfe06d75d433a9ce0dcc57fb583e1161a9
Nick-lorin <chaojie.luan@cygia.com>
2018-01-05 16:57:39 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,16 +1,6 @@
 public class Hello{
-	
-	public static final String COMPANY_NAME="syh科技";
-	public static void main(String[] arg){
-			System.out.println("Hello World，你大爷");
-			System.out.println("这是在子分支brh上修改的");
-			System.out.println("修改了1....");
-			System.out.println("修改了xxxxxx....");
-			print();
-	}
-	public static void print(){
-			System.out.println("***************");
-			System.out.println(" Hello World !");
-			System.out.println("***************");
+	public static void main(String[] args){
+		System.out.println("*******");
+		System.out.println("#######");
 	}
 }
