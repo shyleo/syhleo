c46c5c973c13d8167e7e21691f9cd15da66ec9c6
Nick-lorin <chaojie.luan@cygia.com>
2018-01-05 17:01:46 +0800

    brh hello print

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("*******");
-		System.out.println("#######");
+		System.out.println("brh Hello world");
+		System.out.println("brh hello shyh");
 	}
 }
