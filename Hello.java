4ad5f4ded0e361c1677175cb87ccd89220c8da6c
Nick-lorin <chaojie.luan@cygia.com>
2018-01-05 11:13:34 +0800

    brh:修改了Hello.java文件.

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	
 	public static void main(String[] arg){
 			System.out.println("Hello World，你大爷");
-			
+			System.out.println("这是在子分支brh上修改的");
 			System.out.println("修改了1....");
 			System.out.println("修改了xxxxxx....");
 	}
