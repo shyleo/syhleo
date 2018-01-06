318df20a0c08388bacc20e918507ab390d8e682f
Nick-lorin <chaojie.luan@cygia.com>
2018-01-06 14:14:46 +0800

    Add toString()方法

--- Emp.java
+++ Emp.java
@@ -1,8 +1,13 @@
 import java.io.*;
+import java.util.*;
 public class Emp implements Serializable{
 	private Integer empno;
 	private String ename;
 	private Double sal;
 	private Double comn;
 	private Emp mgr;
+	private Date date;
+	public String toString(){		
+		return "一位雇员";
+	}
 }
